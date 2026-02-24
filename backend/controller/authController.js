30cfe444548b44908d6b228a405200f157ab67bb
shamil <for.photos.only001@gmail.com>
2026-02-24 21:38:21 +0530

    setting up authentication

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,3 +1,3 @@
 const User = require('../model/User')
 
-exports
+exports.Login = ()
--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,16 @@
 require('dotenv').config()
 
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
+app.use(cors({
+    origin:process.env.FRONTEND_URL
+}))
+
+
+
+
+
 const port = process.env.PORT || 3001
-
-
-
 app.listen(port)
